a8f944e4813a4158a5960825a3ea9bc825463743
William <william.teletabs@hotmail.com>
2022-05-02 19:50:33 -0300

    Adicionando evento de click para ativar a função de navegação

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function initTabNav(){
       });
       tabContent[index].classList.add('ativo');
     }
+        
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
