62530917c4cae8e21ba2297e6f8e9a4a0bc26aa9
William <william.teletabs@hotmail.com>
2022-06-20 20:08:48 -0300

    Criando função de scroll

--- script.js
+++ script.js
@@ -45,4 +45,15 @@ initAccordion();
 
 function initScrollSuave(){
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+  
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
 }
